1b2b969f76b79f92991154f88967741531b6ceff
Dima Kerget <dima@kerget.com>
2026-01-24 17:52:01 -0600

    Update thumbnail list

--- src/data/thumbnails.ts
+++ src/data/thumbnails.ts
@@ -1,3 +1,13 @@
 export const THUMBNAILS = [
   "placeholder.jpg",
+  "Baked Salmon.JPG",
+  "BakedCod.JPG",
+  "BrusketWithPearHurma.JPG",
+  "CleanLeanProtein.JPG",
+  "PotatoSkins.JPG",
+  "RingDesert.JPG",
+  "Screenshot_20260124-121832~2.png",
+  "Screenshot_20260124-132623.png",
+  "ShrimpPasta.JPG",
+  "ShrimpToast.JPG",
 ];
