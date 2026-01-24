686a75b4968b318f4096ebf4bb3e104f5f9ca9d0
Dima Kerget <dima@kerget.com>
2026-01-23 21:09:24 -0600

    Add complete Firebase config for production build

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,6 +6,8 @@ const firebaseConfig = {
     apiKey: import.meta.env.VITE_FB_API_KEY,
     authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FB_PROJECT_ID,
+    appId: import.meta.env.VITE_FB_APP_ID,
+    messagingSenderId: import.meta.env.VITE_FB_SENDER_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
